e7f7b378a8590cfb7df490d551fa89d087daae17
Sergey Ivanov <sergivanov@list.ru>
2018-10-09 19:18:52 +0300

    Решил задачу №2 normal

--- lesson03/home_work/hw03_normal.py
+++ lesson03/home_work/hw03_normal.py
@@ -22,9 +22,14 @@
 
 
 def sort_to_max(origin_list):
-    for i, num in enumerate(origin_list):
-        if i < len(origin_list)-1 and origin_list[i] > origin_list[i + 1]:
-            origin_list[i], origin_list[i + 1] = origin_list[i + 1], origin_list[i]
+    n = 1
+    while n < len(origin_list):
+        for i in range(len(origin_list) - n):
+            if origin_list[i] > origin_list[i + 1]:
+                origin_list[i], origin_list[i + 1] = origin_list[i + 1], origin_list[i]
+        n += 1
+    return origin_list
+
 
 print(sort_to_max([2, 10, -12, 2.5, 20, -11, 4, 4, 0]))
 
