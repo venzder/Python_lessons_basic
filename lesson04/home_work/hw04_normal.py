1767f8c946509c7529e34a2f8fb53dfb7c126fb4
Sergey Ivanov <sergivanov@list.ru>
2018-10-17 19:31:27 +0300

    Решил задачу №3 normal

--- lesson04/home_work/hw04_normal.py
+++ lesson04/home_work/hw04_normal.py
@@ -6,6 +6,7 @@
 
 import re
 import random
+import os
 
 line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO' \
        'GIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLeclMwAoktKlfUBJHPsnawvjPhfgewVzK' \
@@ -75,29 +76,29 @@ line_2 = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysm' \
 # result = re.findall(pattern, line_2)
 # print(result)
 
-result_all = []
-i = 2
-while i < len(line):
-    result = []
-    if line[i - 1].islower() and line[i - 2].islower():
-        while i < len(line):
-            if line[i].isupper():
-                result.append(line[i])
-            else:
-                break
-            i += 1
-    i += 1
-    result_all.append(result)
-result_all = [li for li in result_all if len(li) > 3]
-for li in result_all:
-    li.remove(li[-1])
-for li in result_all:
-    li.remove(li[-1])
-result_all = [str(''.join(str(x) for x in li)) for li in result_all]
-result_all = ", ".join(result_all)
-
-
-print(result_all)
+# result_all = []
+# i = 2
+# while i < len(line):
+#     result = []
+#     if line[i - 1].islower() and line[i - 2].islower():
+#         while i < len(line):
+#             if line[i].isupper():
+#                 result.append(line[i])
+#             else:
+#                 break
+#             i += 1
+#     i += 1
+#     result_all.append(result)
+# result_all = [li for li in result_all if len(li) > 3]
+# for li in result_all:
+#     li.remove(li[-1])
+# for li in result_all:
+#     li.remove(li[-1])
+# result_all = [str(''.join(str(x) for x in li)) for li in result_all]
+# result_all = ", ".join(result_all)
+#
+#
+# print(result_all)
 
 
 # Задание-3:
@@ -107,8 +108,20 @@ print(result_all)
 # Найдите и выведите самую длинную последовательность одинаковых цифр
 # в вышезаполненном файле.
 
-# int_1 = [random.randint(0, 9) for x in range(2500)]
-# int_1 = str(int("".join(str(x) for x in int_1)))
-# pattern = r"\{1,}/"
-# result = re.findall(pattern, int_1)
-# print(result)
+path = os.path.join("Test", "test.txt")
+with open(path, "a") as int_1:
+    i = 0
+    while i < 2500:
+        int_1.write(str(random.randint(0, 9)))
+        i += 1
+with open(path, "r") as int_1:
+    for line in int_1:
+        pattern = r"[0]+|[1]+|[2]+|[3]+|[4]+|[5]+|[6]+|[7]+|[8]+|[9]+"
+        result = re.findall(pattern, line)
+    result_max = result[0]
+    for i in result:
+        if len(i) > len(result_max):
+            result_max = i
+    print(result_max)
+
+
