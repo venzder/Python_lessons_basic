f58c5e77e96dda0a1230e259a98ed93597e6bc3c
Sergey Ivanov <sergivanov@list.ru>
2018-10-20 19:27:33 +0300

    Решил задачу №2 easy

--- lesson05/home_work/hw05_easy.py
+++ lesson05/home_work/hw05_easy.py
@@ -4,31 +4,35 @@
 # И второй скрипт, удаляющий эти папки.
 
 import os
-
-i = 1
-while i <= 9:
-    str_1 = "dir_{}".format(i)
-    dir_path = os.path.join(os.getcwd(), str_1)
-    try:
-        os.mkdir(dir_path)
-        print(f"Дирректория {str_1} успешно создана")
-    except FileExistsError:
-        print("Такая директория уже существует")
-    i += 1
-
-i = 1
-while i <= 9:
-    str_del = "dir_{}".format(i)
-    dir_path = os.path.join(os.getcwd(), str_del)
-    try:
-        os.rmdir(dir_path)
-        print(f"Дирректория {str_del} успешно удалена")
-    except FileNotFoundError:
-        print("Дирректория уже удалена или не существует")
-    i += 1
+#
+# i = 1
+# while i <= 9:
+#     str_1 = "dir_{}".format(i)
+#     dir_path = os.path.join(os.getcwd(), str_1)
+#     try:
+#         os.mkdir(dir_path)
+#         print(f"Дирректория {str_1} успешно создана")
+#     except FileExistsError:
+#         print("Такая директория уже существует")
+#     i += 1
+#
+# i = 1
+# while i <= 9:
+#     str_del = "dir_{}".format(i)
+#     dir_path = os.path.join(os.getcwd(), str_del)
+#     try:
+#         os.rmdir(dir_path)
+#         print(f"Дирректория {str_del} успешно удалена")
+#     except FileNotFoundError:
+#         print("Дирректория уже удалена или не существует")
+#     i += 1
 
 # Задача-2:
 # Напишите скрипт, отображающий папки текущей директории.
+
+dir_path = os.path.join(os.getcwd())
+list_dir = [i for i in os.listdir(dir_path) if os.path.isdir(i)]
+print(list_dir)
 
 # Задача-3:
 # Напишите скрипт, создающий копию файла, из которого запущен данный скрипт.
