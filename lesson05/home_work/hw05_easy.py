50d9a7ead8dda6ea1a6868867be54d8a20e5595a
Sergey Ivanov <sergivanov@list.ru>
2018-10-20 18:57:08 +0300

    Решил задачу №1 easy

--- lesson05/home_work/hw05_easy.py
+++ lesson05/home_work/hw05_easy.py
@@ -3,6 +3,30 @@
 # из которой запущен данный скрипт.
 # И второй скрипт, удаляющий эти папки.
 
+import os
+
+i = 1
+while i <= 9:
+    str_1 = "dir_{}".format(i)
+    dir_path = os.path.join(os.getcwd(), str_1)
+    try:
+        os.mkdir(dir_path)
+        print(f"Дирректория {str_1} успешно создана")
+    except FileExistsError:
+        print("Такая директория уже существует")
+    i += 1
+
+i = 1
+while i <= 9:
+    str_del = "dir_{}".format(i)
+    dir_path = os.path.join(os.getcwd(), str_del)
+    try:
+        os.rmdir(dir_path)
+        print(f"Дирректория {str_del} успешно удалена")
+    except FileNotFoundError:
+        print("Дирректория уже удалена или не существует")
+    i += 1
+
 # Задача-2:
 # Напишите скрипт, отображающий папки текущей директории.
 
