b8daa5958624b68b6bb62354b63e68a208aced03
Sergey Ivanov <sergivanov@list.ru>
2018-10-28 20:23:37 +0300

    Решил задачу №2 easy

--- lesson06/home_work/hw06_easy.py
+++ lesson06/home_work/hw06_easy.py
@@ -45,6 +45,8 @@ class Equilateral_trapezoid:
         self.side2 = int(math.sqrt((self.A3[0] - self.A2[0]) ** 2 + (self.A3[1] - self.A2[1]) ** 2))
         self.side3 = int(math.sqrt((self.A4[0] - self.A3[0]) ** 2 + (self.A4[1] - self.A3[1]) ** 2))
         self.side4 = int(math.sqrt((self.A1[0] - self.A4[0]) ** 2 + (self.A1[1] - self.A4[1]) ** 2))
+
+    @property
     def equal(self):
         if self.A2[0] >= self.A3[0]:
             self.A2, self.A3 = self.A3, self.A2
@@ -59,16 +61,42 @@ class Equilateral_trapezoid:
         self.anglekf3 = (self.A2[1] - self.A1[1]) / (self.A2[0] - self.A1[0])
         self.anglekf4 = (self.A3[1] - self.A4[1]) / (self.A3[0] - self.A4[0])
         if self.anglekf1 == self.anglekf2 or self.anglekf3 == self.anglekf4:
-            print("Фигура является трапецией")
             if self.side1 == self.side3 or self.side2 == self.side4:
-                print("Фигура является равнобочной трапецией")
+                return "Фигура является равнобочной трапецией"
+        elif self.anglekf1 == self.anglekf2 or self.anglekf3 == self.anglekf4:
+            return "Фигура является неравнобочной трапецией"
         else:
-            print("Фигура не является трапецией")
+            return "Фигура не является трапецией"
 
+    @property
     def perimeter(self):
         return self.side1 + self.side2 + self.side3 + self.side4
 
-trapezoid1 = Equilateral_trapezoid((12, 24), (16, 46), (25, 46), (29, 24))
-print(trapezoid1.equal())
+    @property
+    def area(self):
+        if self.anglekf1 == self.anglekf2:
+            block1 = (self.side2 + self.side4)/2
+            block2 = (self.side4 - self.side2)**2 + self.side1**2 - self.side3**2
+            block3 = 2 * (self.side4 - self.side2)
+            return block1 * math.sqrt(self.side1**2-(block2 / block3)**2)
+        elif self.anglekf3 == self.anglekf4:
+            block1 = (self.side3 + self.side1) / 2
+            block2 = (self.side3 - self.side1) ** 2 + self.side4 ** 2 - self.side2 ** 2
+            block3 = 2 * (self.side3 - self.side1)
+            return block1 * math.sqrt(self.side4 ** 2 - (block2 / block3) ** 2)
+        else:
+            return "Фигура не является трапецией"
+
+    @property
+    def sides(self):
+        return f"Сторона A1A2 = {self.side1}, Сторона A2A3 = {self.side2}, Сторона A3A4 = {self.side3}, Сторона A4A1 = {self.side4}"
+
+
+
+trapezoid1 = Equilateral_trapezoid((30, 12), (16, 46), (50, 46), (36, 12))
+print(trapezoid1.equal)
+print(trapezoid1.area)
+print(trapezoid1.perimeter)
+print(trapezoid1.sides)
 
 
